c6db024d688790301677cdf408286e3b34989ed8
yannisrachid <yannis.rachid6@gmail.com>
2021-03-23 18:13:36 +0100

    add new data

--- database/abdia.sql
+++ database/abdia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 23 mars 2021 à 13:48
+-- Généré le : mar. 23 mars 2021 à 18:10
 -- Version du serveur :  10.4.14-MariaDB
 -- Version de PHP : 7.4.10
 
@@ -142,6 +142,13 @@ CREATE TABLE `evenement` (
   `date_fin` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `evenement`
+--
+
+INSERT INTO `evenement` (`id_event`, `nom_event`, `date_debut`, `date_fin`) VALUES
+(1, 'Abdia', '2021-03-22', '2021-03-27');
+
 -- --------------------------------------------------------
 
 --
@@ -241,6 +248,102 @@ INSERT INTO `influenceur` (`id_influenceur`, `nom_influenceur`, `id_categorie`) 
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `participer`
+--
+
+CREATE TABLE `participer` (
+  `id_candidature` int(11) NOT NULL,
+  `id_event` int(11) NOT NULL,
+  `id_influenceur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `participer`
+--
+
+INSERT INTO `participer` (`id_candidature`, `id_event`, `id_influenceur`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 1, 4),
+(5, 1, 5),
+(6, 1, 6),
+(7, 1, 7),
+(8, 1, 8),
+(9, 1, 9),
+(10, 1, 10),
+(11, 1, 11),
+(12, 1, 12),
+(13, 1, 13),
+(14, 1, 14),
+(15, 1, 15),
+(16, 1, 16),
+(17, 1, 17),
+(18, 1, 18),
+(19, 1, 19),
+(20, 1, 20),
+(21, 1, 21),
+(22, 1, 22),
+(23, 1, 23),
+(24, 1, 24),
+(25, 1, 25),
+(26, 1, 26),
+(27, 1, 27),
+(28, 1, 28),
+(29, 1, 29),
+(30, 1, 30),
+(31, 1, 31),
+(32, 1, 32),
+(33, 1, 33),
+(34, 1, 34),
+(35, 1, 35),
+(36, 1, 36),
+(37, 1, 37),
+(38, 1, 38),
+(39, 1, 39),
+(40, 1, 40),
+(41, 1, 41),
+(42, 1, 42),
+(43, 1, 43),
+(44, 1, 44),
+(45, 1, 45),
+(46, 1, 46),
+(47, 1, 47),
+(48, 1, 48),
+(49, 1, 49),
+(50, 1, 50),
+(51, 1, 51),
+(52, 1, 52),
+(53, 1, 53),
+(54, 1, 54),
+(55, 1, 55),
+(56, 1, 56),
+(57, 1, 57),
+(58, 1, 58),
+(59, 1, 59),
+(60, 1, 60),
+(61, 1, 61),
+(62, 1, 62),
+(63, 1, 63),
+(64, 1, 64),
+(65, 1, 65),
+(66, 1, 66),
+(67, 1, 67),
+(68, 1, 68),
+(69, 1, 69),
+(70, 1, 70),
+(71, 1, 71),
+(72, 1, 72),
+(73, 1, 73),
+(74, 1, 74),
+(75, 1, 75),
+(76, 1, 76),
+(77, 1, 77),
+(78, 1, 78);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `utilisateur`
 --
 
@@ -290,6 +393,14 @@ ALTER TABLE `influenceur`
   ADD KEY `fk_id_categorie` (`id_categorie`);
 
 --
+-- Index pour la table `participer`
+--
+ALTER TABLE `participer`
+  ADD PRIMARY KEY (`id_candidature`),
+  ADD KEY `participer_ibfk_1` (`id_event`),
+  ADD KEY `participer_ibfk_2` (`id_influenceur`);
+
+--
 -- Index pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
@@ -317,13 +428,19 @@ ALTER TABLE `categorie`
 -- AUTO_INCREMENT pour la table `evenement`
 --
 ALTER TABLE `evenement`
-  MODIFY `id_event` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_event` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `influenceur`
 --
 ALTER TABLE `influenceur`
   MODIFY `id_influenceur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+
+--
+-- AUTO_INCREMENT pour la table `participer`
+--
+ALTER TABLE `participer`
+  MODIFY `id_candidature` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
 
 --
 -- Contraintes pour les tables déchargées
@@ -334,6 +451,13 @@ ALTER TABLE `influenceur`
 --
 ALTER TABLE `influenceur`
   ADD CONSTRAINT `fk_id_categorie` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`);
+
+--
+-- Contraintes pour la table `participer`
+--
+ALTER TABLE `participer`
+  ADD CONSTRAINT `participer_ibfk_1` FOREIGN KEY (`id_event`) REFERENCES `evenement` (`id_event`),
+  ADD CONSTRAINT `participer_ibfk_2` FOREIGN KEY (`id_influenceur`) REFERENCES `influenceur` (`id_influenceur`);
 
 --
 -- Contraintes pour la table `voter`
